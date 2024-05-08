eea7d0d2e86d8b66f09ae4869c2a57cce3abd0fd
viettien1234c <dfgdfgdf11pun0suy@hotmail.com>
2024-05-08 08:22:42 +0700

    feat

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const groupBy = (arr, func) =>
-  arr
-    .map(typeof func === "function" ? func : (val) => val[func])
-    .reduce((acc, val, i) => {
-      acc[val] = (acc[val] || []).concat(arr[i]);
-      return acc;
-    }, {});
+const lcm = (a, b) => {
+  return (a * b) / gcd(a, b);
+};
