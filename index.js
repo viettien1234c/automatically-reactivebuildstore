c1d677c0eb28a6d814b09eb140092cf6fea05bb5
viettien1234c <dfgdfgdf11pun0suy@hotmail.com>
2024-05-15 21:23:41 +0000

    demo task

--- index.js
+++ index.js
@@ -1,14 +1,2 @@
-function findRedundantConnection(edges) {
-  const parent = new Array(edges.length + 1).fill(-1);
-  for (const edge of edges) {
-    const u = find(parent, edge[0]);
-    const v = find(parent, edge[1]);
-    if (u === v) return edge;
-    parent[u] = v;
-  }
-  return [];
-}
-function find(parent, i) {
-  if (parent[i] === -1) return i;
-  return find(parent, parent[i]);
-}
+const initializeArrayWithRange = (end, start = 0) =>
+  Array.from({ length: end - start }, (_, i) => i + start);
